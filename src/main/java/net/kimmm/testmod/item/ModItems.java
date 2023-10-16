516ca4272236de438a8c3be2f912a394b4d857a3
Kim Lin <kimlzx26@gmail.com>
2023-10-15 23:24:14 -0700

    Added recipe for smelting rotten flesh to leather

--- src/main/java/net/kimmm/testmod/item/ModItems.java
+++ src/main/java/net/kimmm/testmod/item/ModItems.java
@@ -14,8 +14,7 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
 
-    public static final RegistryObject<Item> STEEL = ITEMS.register("steel",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
